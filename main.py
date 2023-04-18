a942093a395c793909d9dc5684842144759a90b6
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-18 19:04:24 +0200

    Add: To Customer funct -> "off" machine opt -> Report opt to print resources from coffee machine Add: Resources check funct where we check if machine have enough resources to make a drink.

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import random
 
 def customer_input():
     customer_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if customer_choice == "off":
+        print("")
+    elif customer_choice == "report":
+        for i in data.resources:
+            if i == "coffee":
+                print(f"{i}: {data.resources[i]}g")
+            else:
+                print(f"{i}: {data.resources[i]}ml")
     return customer_choice
+
+print()
+def resources_sufficient(customer_answer):
+    if customer_answer == "espresso":
+        if data.resources["water"] > data.MENU["espresso"]["ingredients"]["water"]:
+            print("Oto espresso!")
+
 customer_answer = customer_input()
+resources_sufficient(customer_answer)
+
 
