ce559a7cd2f5922d83ca41f6335d43dd05c387b7
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-18 18:49:06 +0200

    Initial Commit for DATA

---
+++ main.py
@@ -0,0 +1,8 @@
+import data
+import random
+
+
+def customer_input():
+    customer_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    return customer_choice
+customer_answer = customer_input()
