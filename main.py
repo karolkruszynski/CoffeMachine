03f731dd586976f37e46faad3f2ec0d963db487b
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-19 21:23:21 +0200

    Bug fix: Add money symbol as $ not ml as ealier Bug fix: Repair error for customer change from buy Add: Current credits info when user put coins in machine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import data
 import random
 
-
 def customer_input():
     customer_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if customer_choice == "off":
@@ -10,6 +9,8 @@ def customer_input():
         for i in data.resources:
             if i == "coffee":
                 print(f"{i}: {data.resources[i]}g")
+            elif i == "money":
+                print(f"{i}: {data.resources[i]}$")
             else:
                 print(f"{i}: {data.resources[i]}ml")
     return customer_choice
@@ -20,14 +21,14 @@ def resources_sufficient(customer_answer):
     if customer_answer == "espresso":
         if data.resources["water"] > data.MENU["espresso"]["ingredients"]["water"]:
             if data.resources["coffee"] > data.MENU["espresso"]["ingredients"]["coffee"]:
-                print("Oto ESPRESSO!")
+                print("Enough resources")
+                is_resources = True
                 data.resources["water"] -= 50
                 data.resources["coffee"] -= 18
                 data.resources["money"] += 1.5
-                print(data.resources["water"])
-                print(data.resources["coffee"])
-                print(data.resources["money"])
-                is_resources = True
+                # print(data.resources["water"])
+                # print(data.resources["coffee"])
+                # print(data.resources["money"])
             else:
                     print("Sorry there is not enough coffee.")
         else:
@@ -36,15 +37,16 @@ def resources_sufficient(customer_answer):
         if data.resources["water"] > data.MENU["latte"]["ingredients"]["water"]:
             if data.resources["coffee"] > data.MENU["latte"]["ingredients"]["coffee"]:
                 if data.resources["coffee"] > data.MENU["latte"]["ingredients"]["coffee"]:
-                    print("Oto LATTE!")
+                    print("Enough resources")
+                    is_resources = True
                     data.resources["water"] -= 200
                     data.resources["milk"] -= 150
                     data.resources["coffee"] -= 24
                     data.resources["money"] += 2.5
-                    print(data.resources["water"])
-                    print(data.resources["milk"])
-                    print(data.resources["coffee"])
-                    print(data.resources["money"])
+                    # print(data.resources["water"])
+                    # print(data.resources["milk"])
+                    # print(data.resources["coffee"])
+                    # print(data.resources["money"])
                 else:
                         print("Sorry there is not enough coffee.")
             else:
@@ -55,15 +57,16 @@ def resources_sufficient(customer_answer):
         if data.resources["water"] > data.MENU["cappuccino"]["ingredients"]["water"]:
             if data.resources["coffee"] > data.MENU["cappuccino"]["ingredients"]["coffee"]:
                 if data.resources["coffee"] > data.MENU["cappuccino"]["ingredients"]["coffee"]:
-                    print("Oto CAPUCCINO!")
+                    print("Enough resources")
+                    is_resources = True
                     data.resources["water"] -= 250
                     data.resources["milk"] -= 100
                     data.resources["coffee"] -= 24
                     data.resources["money"] += 3.0
-                    print(data.resources["water"])
-                    print(data.resources["milk"])
-                    print(data.resources["coffee"])
-                    print(data.resources["money"])
+                    # print(data.resources["water"])
+                    # print(data.resources["milk"])
+                    # print(data.resources["coffee"])
+                    # print(data.resources["money"])
                 else:
                     print("Sorry there is not enough coffee.")
             else:
@@ -73,17 +76,30 @@ def resources_sufficient(customer_answer):
 
 
 def process_coins(customer_answer,is_resources):
+    credits = 0.0
+    print(f"Yours credits: {credits}$")
+
     quarters = int(input("How much quarters do you have?: "))
+    credits = quarters * 0.25
+    print(f"Yours credits: {credits}$")
     dimes = int(input("How much dimes do you have?: "))
+    credits += dimes * 0.10
+    print(f"Yours credits: {credits}$")
     nickles = int(input("How much nickles do you have?: "))
+    credits += nickles * 0.05
+    print(f"Yours credits: {credits}$")
     pennies = int(input("How much pennies do you have?: "))
+    credits += pennies * 0.01
+    print(f"Yours credits: {credits}$")
+
     sum_of_customer_money = (quarters*0.25)+(dimes*0.10)+(nickles*0.05)+(pennies*0.01)
-    return_for_customer = 0
-    espresso_cost = data.MENU["espresso"]["cost"]
+    return_for_customer = 0.0
+    espresso_cost = data.MENU["espresso"]["cost"]   # coast jest jako float - do poprawy!
     if customer_answer == "espresso" and is_resources == True:
         if sum_of_customer_money > data.MENU["espresso"]["cost"]:
-            # sum_of_customer_money - espresso_cost = return_for_customer
-            print('x')
+            return_for_customer = round(sum_of_customer_money - espresso_cost,2)
+            print(f"Please this is yours {customer_answer}.")
+            print(f"And this is yours change {return_for_customer}$")
 
 test = False
 while test == False:
